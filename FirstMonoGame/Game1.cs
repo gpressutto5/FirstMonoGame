1c61023556ddf7defe8eb04a06f830f43f50e00f
Guilherme Pressutto <gpressutto5@gmail.com>
2019-02-13 15:33:03 -0200

    Added bouncing dvd logo

--- FirstMonoGame/Game1.cs
+++ FirstMonoGame/Game1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Diagnostics;
+using System.Runtime.Remoting.Messaging;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -13,6 +14,17 @@ namespace FirstMonoGame
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+
+		private Texture2D _dvd;
+		private float _targetX = 128;
+		private float _targetY;
+		private Vector2 _scale;
+
+		private int _speed = 200;
+		private Vector2 _position = new Vector2(0, 0);
+		private Vector2 _velocity;
+		
+		private Color _color;
 
 		public Game1()
 		{
@@ -42,7 +54,11 @@ namespace FirstMonoGame
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
-			//TODO: use this.Content to load your game content here 
+			_velocity = new Vector2(_speed, _speed);
+			_dvd = this.Content.Load<Texture2D>("dvd");
+			_scale = new Vector2(_targetX / _dvd.Width, _targetX / _dvd.Width);
+			_targetY = _dvd.Height * _scale.Y;
+			ChangeColor();
 		}
 
 		/// <summary>
@@ -59,7 +75,29 @@ namespace FirstMonoGame
 				Exit();
 #endif
 
-			// TODO: Add your update logic here
+			_position += _velocity * (float) gameTime.ElapsedGameTime.TotalSeconds;
+
+			if (_position.X <= 0 && _velocity.X < 0)
+			{
+				ChangeColor();	
+				_velocity.X = _speed;
+			}
+			if (_position.Y <= 0 && _velocity.Y < 0)
+			{
+				ChangeColor();	
+				_velocity.Y = _speed;
+			}
+
+			if (_position.X + _targetX >= graphics.GraphicsDevice.Viewport.Width && _velocity.X > 0)
+			{
+				ChangeColor();	
+				_velocity.X = -_speed;
+			}
+			if (_position.Y + _targetY >= graphics.GraphicsDevice.Viewport.Height && _velocity.Y > 0)
+			{
+				ChangeColor();	
+				_velocity.Y = -_speed;
+			}
 
 			base.Update(gameTime);
 		}
@@ -70,11 +108,19 @@ namespace FirstMonoGame
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-
-			//TODO: Add your drawing code here
+			graphics.GraphicsDevice.Clear(Color.Black);
+	
+			spriteBatch.Begin();
+			spriteBatch.Draw(_dvd, _position, color: _color, scale: _scale);
+			spriteBatch.End();
 
 			base.Draw(gameTime);
+		}
+
+		private void ChangeColor()
+		{
+			var random = new Random();
+			_color = new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
 		}
 	}
 }
